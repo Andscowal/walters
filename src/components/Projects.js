f1e00325a4a8eb2cc47754bde2d44f16627cd751
Andrew Walters <andscowal@gmail.com>
2018-03-18 10:57:53 -0700

    Updated projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,19 +8,31 @@ class Projects extends Component {
         <h1 className="heading">My Projects</h1>
         <div className="projects">
           <div className="prj">
-            <img src={require("../img/aus.jpeg")} />
-            <div className="text">Coming Soon</div>
+            <img src={require("../img/github-battle.png")} />
+            <div className="text">
+              <a href="https://github-battle-78b78-3d20f.firebaseapp.com/">
+                Github Battle
+              </a>
+            </div>
           </div>
           <div className="prj">
-            <img src={require("../img/comp.jpeg")} />
-            <div className="text">Coming Soon</div>
+            <img src={require("../img/js-drums.png")} />
+            <div className="text">
+              <a href="https://andscowal.github.io/js-drums/">
+                JavaScript Drums
+              </a>
+            </div>
+          </div>
+          <div className="prj">
+            <img src={require("../img/js-clock.png")} />
+            <div className="text">
+              <a href="https://andscowal.github.io/js-clock/">
+                JavaScript Clock
+              </a>
+            </div>
           </div>
           <div className="prj">
             <img src={require("../img/fall.jpg")} />
-            <div className="text">Coming Soon</div>
-          </div>
-          <div className="prj">
-            <img src={require("../img/flower.jpeg")} />
             <div className="text">Coming Soon</div>
           </div>
         </div>
